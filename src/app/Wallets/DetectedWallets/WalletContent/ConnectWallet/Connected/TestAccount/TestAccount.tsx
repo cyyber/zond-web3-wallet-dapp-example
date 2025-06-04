1a9a4aa117f5d06804944727496a356fa84483f2
Muhammed Shaheer Keerirakath <muhammed.shaheer.keerirakath@gmail.com>
2025-06-04 08:20:57 +0400

    fix: updates account

--- src/app/Wallets/DetectedWallets/WalletContent/ConnectWallet/Connected/TestAccount/TestAccount.tsx
+++ src/app/Wallets/DetectedWallets/WalletContent/ConnectWallet/Connected/TestAccount/TestAccount.tsx
@@ -1,3 +1,8 @@
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import { Copy } from "lucide-react";
 
 type TestAccountProps = {
@@ -14,23 +19,37 @@ export const TestAccount = ({ title, account, seed }: TestAccountProps) => {
   return (
     <div className="space-y-2">
       <div>{title}</div>
-      <div>
-        <div className="flex gap-2 justify-between">
+      <div className="flex flex-col gap-1">
+        <div className="flex gap-2 items-center justify-between">
           <span className="font-bold break-all">{account}</span>
           <span>
-            <Copy
-              className="cursor-pointer w-4"
-              onClick={() => copyText(account || "")}
-            />
+            <Tooltip>
+              <TooltipTrigger>
+                <Copy
+                  className="cursor-pointer"
+                  onClick={() => copyText(account || "")}
+                />
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Copy</p>
+              </TooltipContent>
+            </Tooltip>
           </span>
         </div>
-        <div className="flex gap-2 justify-between">
+        <div className="flex gap-2 items-center justify-between">
           <span>{seed}</span>
           <span>
-            <Copy
-              className="cursor-pointer w-4"
-              onClick={() => copyText(seed || "")}
-            />
+            <Tooltip>
+              <TooltipTrigger>
+                <Copy
+                  className="cursor-pointer"
+                  onClick={() => copyText(seed || "")}
+                />
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Copy</p>
+              </TooltipContent>
+            </Tooltip>
           </span>
         </div>
       </div>

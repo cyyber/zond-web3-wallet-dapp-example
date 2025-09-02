7a37f05f285a78aa59103c32d1fa922c4c9feef2
Muhammed Shaheer Keerirakath <muhammed.shaheer.keerirakath@gmail.com>
2025-09-02 14:24:53 +0400

    feat: updates ui

--- src/app/Wallets/Wallets.tsx
+++ src/app/Wallets/Wallets.tsx
@@ -1,6 +1,3 @@
-import { ThemeProvider } from "@/contexts/themeProviderContext";
-import { DetectedWallets } from "./DetectedWallets/DetectedWallets";
-import { SelectedWallet } from "./SelectedWallet/SelectedWallet";
 import {
   Card,
   CardContent,
@@ -8,33 +5,30 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { DetectedWallets } from "./DetectedWallets/DetectedWallets";
+import { SelectedWallet } from "./SelectedWallet/SelectedWallet";
 import { WalletError } from "./WalletError/WalletError";
-import { WalletProvider } from "./WalletProvider";
 import { WalletResponse } from "./WalletResponse/WalletResponse";
 
 export const Wallets = () => {
   return (
-    <ThemeProvider>
-      <WalletProvider>
-        <Card className="m-16">
-          <CardHeader>
-            <CardTitle>Wallets Detected</CardTitle>
-            <CardDescription>
-              The web3 wallet extensions detected on this browser
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="grid grid-cols-3 gap-6">
-            <div className="col-span-2">
-              <DetectedWallets />
-            </div>
-            <div className="col-span-1 space-y-6">
-              <SelectedWallet />
-              <WalletResponse />
-              <WalletError />
-            </div>
-          </CardContent>
-        </Card>
-      </WalletProvider>
-    </ThemeProvider>
+    <Card className="m-16 max-w-[80rem]">
+      <CardHeader>
+        <CardTitle>Wallets Detected</CardTitle>
+        <CardDescription>
+          The web3 wallet extensions detected on this browser
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="grid grid-cols-3 gap-6">
+        <div className="col-span-2">
+          <DetectedWallets />
+        </div>
+        <div className="col-span-1 space-y-6">
+          <SelectedWallet />
+          <WalletResponse />
+          <WalletError />
+        </div>
+      </CardContent>
+    </Card>
   );
 };
